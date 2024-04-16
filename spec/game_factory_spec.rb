40fc393388cb44241dfeb26adf3a1d959d64512d
Noah Durbin <noahdurbin@gmail.com>
2024-04-16 17:06:34 -0600

    feat: creates game objects

--- spec/game_factory_spec.rb
+++ spec/game_factory_spec.rb
@@ -4,9 +4,9 @@ RSpec.describe GameFactory do
   describe 'creates Game objects from CSV data' do
     it 'creates Game objects' do
       factory = GameFactory.new
-      games = factory.create_games('./fixtures/games_fixtures.csv')
-
-      expect(games).to all be_a(Game)
+      games = factory.create_games("./fixtures/games_fixture.csv")
+# require 'pry'; binding.pry
+      expect(factory.games).to all be_a(Game)
     end
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,18 +3,18 @@ require 'spec_helper'
 RSpec.describe Game do
   describe 'initialize' do
     it 'exists' do
-      game1 = Game.new({game_id: 2012030221, season: 20122013, type: :Postseason, date_time: '2013-05-16', away_team_id: 3, home_team_id: 6, away_goals: 2, home_goals: 3, venue: 'Toyota Stadium', venue_link: '/api/v1/venues/null'})
+      game1 = Game.new({game_id: 2012030221, season: 20122013, type: :Postseason, date_time: 5/16/13, away_team_id: 3, home_team_id: 6, away_goals: 2, home_goals: 3, venue: 'Toyota Stadium', venue_link: '/api/v1/venues/null'})
 
       expect(game1).to be_a(Game)
     end
 
     it 'has the correct data types in attributes' do
-      game1 = Game.new({game_id: 2012030221, season: 20122013, type: :Postseason, date_time: '2013-05-16', away_team_id: 3, home_team_id: 6, away_goals: 2, home_goals: 3, venue: 'Toyota Stadium', venue_link: '/api/v1/venues/null'})
+      game1 = Game.new({game_id: 2012030221, season: 20122013, type: :Postseason, date_time: 5/16/13, away_team_id: 3, home_team_id: 6, away_goals: 2, home_goals: 3, venue: 'Toyota Stadium', venue_link: '/api/v1/venues/null'})
 
       expect(game1.game_id).to be_a(Integer)
       expect(game1.season).to be_a(Integer)
       expect(game1.type).to be_a(Symbol)
-      expect(game1.date_time).to be_a(String)
+      expect(game1.date_time).to be_a(Integer)
       expect(game1.away_team_id).to be_a(Integer)
       expect(game1.home_team_id).to be_a(Integer)
       expect(game1.away_goals).to be_a(Integer)
