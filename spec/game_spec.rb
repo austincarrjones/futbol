2ae8721badd6337bb6e5c63f96c832a62af21bf2
Noah Durbin <noahdurbin@gmail.com>
2024-04-16 15:49:25 -0600

    feat: initializes as a game

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,9 +3,9 @@ require 'spec_helper'
 RSpec.describe Game do
   describe 'initialize' do
     it 'exists' do
-      game = Game.new(2012030221,20122013,Postseason,5/16/13,3,6,2,3,Toyota Stadium,/api/v1/venues/null)
+      game1 = Game.new({game_id: 2012030221, season: 20122013, type: :Postseason, date_time: '2013-05-16', away_team_id: 3, home_team_id: 6, away_goals: 2, home_goals: 3, venue: 'Toyota Stadium', venue_link: '/api/v1/venues/null'})
 
-      expect(game).to be_a(Game)
+      expect(game1).to be_a(Game)
     end
   end
 end
