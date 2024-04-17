00049d0d03e5f60b96585515a9db206a36a2b9ae
Noah Durbin <13364668+noahdurbin@users.noreply.github.com>
2024-04-17 16:17:10 -0600

    test: tests for percentage of wins based on home or away

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -60,20 +60,27 @@ RSpec.describe Game do
       factory = GameFactory.new
       factory.create_games("./fixtures/games_fixture.csv")
 
-
+      expect(Game.percentage_home_wins).to eq(75.00)
     end
 
     it 'can calculate percentage of away wins' do
       factory = GameFactory.new
       factory.create_games("./fixtures/games_fixture.csv")
 
-
+      expect(Game.percentage_away_wins).to eq(20.00)
     end
 
     it 'can tell if a home team won or away team won' do
       game1 = Game.new({game_id: 2012030221, season: 20122013, type: :Postseason, date_time: 5/16/13, away_team_id: 3, home_team_id: 6, away_goals: 2, home_goals: 3, venue: 'Toyota Stadium', venue_link: '/api/v1/venues/null'})
 
       expect(game1.home_win?).to eq(true)
+    end
+
+    it 'can return a count of all games' do
+      factory = GameFactory.new
+      factory.create_games("./fixtures/games_fixture.csv")
+
+      expect(Game.game_count).to eq(20)
     end
   end
 end
