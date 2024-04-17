883b8fee52a9d02d7693a5156bdbdbe666a14cd5
Zach Bergman <bzach634@gmail.com>
2024-04-16 20:59:19 -0600

    test: Add #all_game_teams test

--- spec/game_team_factory_spec.rb
+++ spec/game_team_factory_spec.rb
@@ -16,4 +16,10 @@ RSpec.describe GameTeamFactory do
       expect(game_teams).to all be_a(GameTeam)
     end
   end
+
+  describe "#all_game_teams" do
+    it "returns an array of all GameTeam objects created" do
+      expect(GameTeamFactory.all_game_teams).to all be_a(GameTeam)
+    end
+  end
 end
