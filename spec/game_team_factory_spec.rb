cb7e8502543cf5b5e4a887038e665afc9c41b70a
Zach Bergman <bzach634@gmail.com>
2024-04-16 20:21:05 -0600

    test: Edit name of #coach_game_count helper test

--- spec/game_team_factory_spec.rb
+++ spec/game_team_factory_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe GameTeamFactory do
   describe "#initialize" do
     it "exists" do
       factory = GameTeamFactory.new
-      
+
       expect(factory).to be_a GameTeamFactory
     end
   end
@@ -14,6 +14,20 @@ RSpec.describe GameTeamFactory do
       game_teams = GameTeamFactory.create_from_csv("./fixtures/game_teams_fixture.csv")
 
       expect(game_teams).to all be_a(GameTeam)
+    end
+  end
+
+  describe "#coach_game_count - helper" do
+    it "returns a Hash of the number of games a coach played for a specified season" do
+      expected = {
+        "John Tortorella" => 5,
+        "Claude Julien" => 9,
+        "Dan Bylsma" => 4,
+        "Mike Babcock" => 1,
+        "Joel Quenneville" => 1
+      }
+
+      expect(GameTeam.coach_game_count("2012")).to eq(expected)
     end
   end
 end
