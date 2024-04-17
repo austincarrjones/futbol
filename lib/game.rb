0258e2fdef99924b5fecf502ecbf3393821b4fd8
Noah Durbin <noahdurbin@gmail.com>
2024-04-17 15:16:31 -0600

    fix: data types in the game class

--- lib/game.rb
+++ lib/game.rb
@@ -6,12 +6,12 @@ class Game
               :venue, :venue_link
   
   def initialize(game_data)
-    @game_id = game_data[:game_id]
-    @season = game_data[:season]
+    @game_id = game_data[:game_id].to_i
+    @season = game_data[:season].to_i
     @type = game_data[:type]
-    @date_time = game_data[:date_time]
-    @away_team_id = game_data[:away_team_id]
-    @home_team_id = game_data[:home_team_id]
+    @date_time = game_data[:date_time].to_i
+    @away_team_id = game_data[:away_team_id].to_i
+    @home_team_id = game_data[:home_team_id].to_i
     @away_goals = game_data[:away_goals].to_i
     @home_goals = game_data[:home_goals].to_i
     @venue = game_data[:venue]
