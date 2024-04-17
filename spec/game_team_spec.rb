d9dc5941e5206b58cc021decda1ec655fdc7bd8d
Zach Bergman <bzach634@gmail.com>
2024-04-16 21:04:36 -0600

    refactor: Fix indentation of tests

--- spec/game_team_spec.rb
+++ spec/game_team_spec.rb
@@ -20,31 +20,31 @@ RSpec.describe GameTeam do
         takeaways: 7
     }
     @gameteam_attr_test = GameTeam.new(gameteam_data)
-end
-
-describe "initialize" do
-    it "exists" do
-        expect(@gameteam_attr_test).to be_an_instance_of(GameTeam)
-    end
-
-    it "has readable attributes" do
-        expect(@gameteam_attr_test.game_id).to eq("2012030221")
-        expect(@gameteam_attr_test.team_id).to eq("3")
-        expect(@gameteam_attr_test.hoa).to eq("away")
-        expect(@gameteam_attr_test.result).to eq("LOSS")
-        expect(@gameteam_attr_test.settled_in).to eq("OT")
-        expect(@gameteam_attr_test.head_coach).to eq("John Tortorella")
-        expect(@gameteam_attr_test.goals).to eq(2)
-        expect(@gameteam_attr_test.shots).to eq(8)
-        expect(@gameteam_attr_test.tackles).to eq(44)
-        expect(@gameteam_attr_test.pim).to eq(8)
-        expect(@gameteam_attr_test.ppo).to eq(3)
-        expect(@gameteam_attr_test.ppg).to eq(0)
-        expect(@gameteam_attr_test.faceoff_win_per).to eq(44.8)
-        expect(@gameteam_attr_test.giveaways).to eq(17)
-        expect(@gameteam_attr_test.takeaways).to eq(7)
-    end
   end
+
+  describe "initialize" do
+      it "exists" do
+          expect(@gameteam_attr_test).to be_an_instance_of(GameTeam)
+      end
+
+      it "has readable attributes" do
+          expect(@gameteam_attr_test.game_id).to eq("2012030221")
+          expect(@gameteam_attr_test.team_id).to eq("3")
+          expect(@gameteam_attr_test.hoa).to eq("away")
+          expect(@gameteam_attr_test.result).to eq("LOSS")
+          expect(@gameteam_attr_test.settled_in).to eq("OT")
+          expect(@gameteam_attr_test.head_coach).to eq("John Tortorella")
+          expect(@gameteam_attr_test.goals).to eq(2)
+          expect(@gameteam_attr_test.shots).to eq(8)
+          expect(@gameteam_attr_test.tackles).to eq(44)
+          expect(@gameteam_attr_test.pim).to eq(8)
+          expect(@gameteam_attr_test.ppo).to eq(3)
+          expect(@gameteam_attr_test.ppg).to eq(0)
+          expect(@gameteam_attr_test.faceoff_win_per).to eq(44.8)
+          expect(@gameteam_attr_test.giveaways).to eq(17)
+          expect(@gameteam_attr_test.takeaways).to eq(7)
+      end
+    end
 
   describe "GameTeam Class Methods" do
     before(:all) do
@@ -60,7 +60,7 @@ describe "initialize" do
           "Mike Babcock" => 1,
           "Joel Quenneville" => 1
         }
-        
+
         expect(GameTeam.coach_game_count("20122013")).to eq(expected)
       end
     end
