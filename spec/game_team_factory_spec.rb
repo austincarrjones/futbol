383d2845bbcbb6e905345d321d3227434ba12abd
Zach Bergman <bzach634@gmail.com>
2024-04-16 20:22:14 -0600

    refactor: Move #coach_game_count test to game_team_spec

--- spec/game_team_factory_spec.rb
+++ spec/game_team_factory_spec.rb
@@ -16,18 +16,4 @@ RSpec.describe GameTeamFactory do
       expect(game_teams).to all be_a(GameTeam)
     end
   end
-
-  describe "#coach_game_count - helper" do
-    it "returns a Hash of the number of games a coach played for a specified season" do
-      expected = {
-        "John Tortorella" => 5,
-        "Claude Julien" => 9,
-        "Dan Bylsma" => 4,
-        "Mike Babcock" => 1,
-        "Joel Quenneville" => 1
-      }
-
-      expect(GameTeam.coach_game_count("2012")).to eq(expected)
-    end
-  end
 end
--- spec/game_team_spec.rb
+++ spec/game_team_spec.rb
@@ -45,4 +45,18 @@ describe "initialize" do
         expect(@gameteam_attr_test.takeaways).to eq(7)
     end
   end
+
+  describe "#coach_game_count - helper" do
+    it "returns a Hash of the number of games a coach played for a specified season" do
+      expected = {
+        "John Tortorella" => 5,
+        "Claude Julien" => 9,
+        "Dan Bylsma" => 4,
+        "Mike Babcock" => 1,
+        "Joel Quenneville" => 1
+      }
+
+      expect(GameTeam.coach_game_count("2012")).to eq(expected)
+    end
+  end
 end
