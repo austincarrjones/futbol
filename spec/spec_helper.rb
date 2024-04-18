78c0808c28460114d34b7bb20cb9120f38baa809
Zach Bergman <bzach634@gmail.com>
2024-04-18 11:33:08 -0600

    refactor: Require files in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,12 @@
 require 'simplecov'
+require 'csv'
+require 'pry'
+require "./lib/game_team_factory"
+require "./lib/game_team"
+require "./lib/stat_tracker"
 
 SimpleCov.start
 
 RSpec.configure do |config|
     config.formatter = :documentation
 end
-
-require 'pry'
-require "./lib/game_team_factory"
-require "./lib/game_team"
