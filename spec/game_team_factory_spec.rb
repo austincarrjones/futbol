7009dd32a442ba902db712da4d4c6ff0c91c69b0
Zach Bergman <bzach634@gmail.com>
2024-04-18 14:40:35 -0600

    refactor: Change name of ::create_from_csv to ::create_game_teams

--- spec/game_team_factory_spec.rb
+++ spec/game_team_factory_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe GameTeamFactory do
 
   describe "#create_from_csv" do
     it "create GameTeam objects from csv file" do
-      game_teams = GameTeamFactory.create_from_csv("./fixtures/game_teams_fixture.csv")
+      game_teams = GameTeamFactory.create_game_teams("./fixtures/game_teams_fixture.csv")
 
       expect(game_teams).to all be_a(GameTeam)
     end
