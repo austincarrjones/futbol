a8fcd251217ba1934f545662704a941bd7914302
Zach Bergman <bzach634@gmail.com>
2024-04-19 12:38:00 -0600

    test: Add ::tackles_per_team helper test

--- spec/game_team_spec.rb
+++ spec/game_team_spec.rb
@@ -1,59 +1,41 @@
 require "spec_helper"
 
 RSpec.describe GameTeam do
-  before(:each) do
-    gameteam_data = {
-        game_id: "2012030221",
-        team_id: "3",
-        hoa: "away",
-        result: "LOSS",
-        settled_in: "OT",
-        head_coach: "John Tortorella",
-        goals: 2,
-        shots: 8,
-        tackles: 44,
-        pim: 8,
-        ppo: 3,
-        ppg: 0,
-        faceoff_win_per: 44.8,
-        giveaways: 17,
-        takeaways: 7
-    }
-    @gameteam_attr_test = GameTeam.new(gameteam_data)
-  end
 
   describe "initialize" do
       it "exists" do
-          expect(@gameteam_attr_test).to be_an_instance_of(GameTeam)
-      end
+          gameteam_data = {
+          game_id: "2012030221",
+          team_id: "3",
+          hoa: "away",
+          result: "LOSS",
+          settled_in: "OT",
+          head_coach: "John Tortorella",
+          goals: 2,
+          shots: 8,
+          tackles: 44,
+          pim: 8,
+          ppo: 3,
+          ppg: 0,
+          faceoff_win_per: 44.8,
+          giveaways: 17,
+          takeaways: 7
+        }   
 
-      it "has readable attributes" do
-          expect(@gameteam_attr_test.game_id).to eq("2012030221")
-          expect(@gameteam_attr_test.team_id).to eq("3")
-          expect(@gameteam_attr_test.hoa).to eq("away")
-          expect(@gameteam_attr_test.result).to eq("LOSS")
-          expect(@gameteam_attr_test.settled_in).to eq("OT")
-          expect(@gameteam_attr_test.head_coach).to eq("John Tortorella")
-          expect(@gameteam_attr_test.goals).to eq(2)
-          expect(@gameteam_attr_test.shots).to eq(8)
-          expect(@gameteam_attr_test.tackles).to eq(44)
-          expect(@gameteam_attr_test.pim).to eq(8)
-          expect(@gameteam_attr_test.ppo).to eq(3)
-          expect(@gameteam_attr_test.ppg).to eq(0)
-          expect(@gameteam_attr_test.faceoff_win_per).to eq(44.8)
-          expect(@gameteam_attr_test.giveaways).to eq(17)
-          expect(@gameteam_attr_test.takeaways).to eq(7)
+          @gameteam_test = GameTeam.new(gameteam_data)
+
+          expect(@gameteam_test).to be_an_instance_of(GameTeam)
       end
     end
 
   describe "GameTeam Class Methods" do
     before(:all) do
-        GameTeamFactory.create_from_csv("./fixtures/game_teams_fixture.csv")
-        # GameTeamFactory.create_from_csv("./data/game_teams.csv")
+        GameTeamFactory.create_game_teams("./fixtures/game_teams_fixture.csv")
+        # GameTeamFactory.create_game_teams("./data/game_teams.csv")
         #run with real data too
     end
 
-    describe "#all_game_teams" do
+    describe "::all_game_teams" do
       it "returns all game_team objects from Factory" do
         expect(GameTeam.all_game_teams).to be_a(Array)
         expect(GameTeam.all_game_teams.length).to eq(78)
@@ -110,17 +92,30 @@ RSpec.describe GameTeam do
       end
     end
 
-    describe "#winningest_coach" do
+    describe "::winningest_coach" do
       it "returns the coach's name with the highest win percentage for specified season" do
         expect(GameTeam.winningest_coach("20132014")).to eq("Mike Yeo")
         expect(GameTeam.winningest_coach("20142015")).to eq("Alain Vigneault")
       end
     end
 
-    describe "#worst_coach" do
+    describe "::worst_coach" do
       it "returns the coach's name with the lowest win percentage for specified season" do
         expect(GameTeam.worst_coach("20132014")).to eq("Patrick Roy")
         expect(GameTeam.worst_coach("20142015")).to eq("Mike Johnston")
+      end
+    end
+
+    describe "::tackles_per_team" do
+      it "returns the name of the team and the amount of tackles for each - 20132014" do
+        expected = {
+          "FC Cincinnati" => 299,
+          "Orlando City SC" => 194,
+          "Real Salt Lake" => 272,
+          "Vancouver Whitecaps FC" => 188
+        }
+
+        expect(GameTeam.tackles_per_team("20132014")).to eq(expected)
       end
     end
   end
