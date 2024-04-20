9481bfd3d2d41122ccf2f9a52eddb6418b14445d
Austin Carr-Jones <austincarr.jones@gmail.com>
2024-04-20 16:59:38 -0600

    fix: must have deleted some tests in game_team_spec somehow. yikes. restored.

--- spec/game_team_spec.rb
+++ spec/game_team_spec.rb
@@ -43,7 +43,7 @@ RSpec.describe GameTeam do
       end
     end
 
-    describe "#coach_game_count - helper" do
+    describe "::coach_game_count - helper" do
       it "returns a Hash of the number of games a coach played for a specified season" do
         expected = {
           "Bruce Boudreau" => 7,
@@ -68,7 +68,7 @@ RSpec.describe GameTeam do
       end
     end
 
-    describe "#coach_win_count - helper" do
+    describe "::coach_win_count - helper" do
       it "returns a Hash of the number of games a coach won for a specified season" do
         expected = {
           "Bruce Boudreau" => 2,
@@ -106,7 +106,7 @@ RSpec.describe GameTeam do
       end
     end
 
-    describe "#tackles_per_team - helper" do
+    describe "::tackles_per_team - helper" do
       it "returns the name of the team and the amount of tackles for each - 20132014" do
         expected = {
           "FC Cincinnati" => 299,
@@ -133,6 +133,126 @@ RSpec.describe GameTeam do
       end
     end
 
+    describe "::team_id_and_shots - helper" do
+      it "returns a hash containing team id pointing to amount of shots for 20132014 season" do
+        expected = {
+          30=>56, 
+          21=>40, 
+          26=>49, 
+          24=>42
+        }
+
+        expect(GameTeam.team_id_and_shots("20132014")).to eq(expected)
+      end
+
+      it "returns a hash containing team id pointing to amount of shots for 20142015 season" do
+        expected = {
+          5=>25, 
+          3=>75, 
+          14=>51, 
+          30=>32, 
+          19=>37, 
+          9=>47, 
+          8=>54
+        }
+
+        expect(GameTeam.team_id_and_shots("20142015")).to eq(expected)
+      end
+    end
+
+    describe "::team_id_and_goals - helper" do
+      it "returns a hash containing team id pointing to amount of goals for 20132014 season" do
+        expected = {
+          30=>16, 
+          21=>12, 
+          26=>17, 
+          24=>13
+        }
+
+        expect(GameTeam.team_id_and_goals("20132014")).to eq(expected)
+      end
+
+      it "returns a hash containg team id pointing to amount of goals for 20142015 season" do
+        expected = {
+          5=>5, 
+          3=>24, 
+          14=>17, 
+          30=>11, 
+          19=>10, 
+          9=>10, 
+          8=>10
+        }
+
+        expect(GameTeam.team_id_and_goals("20142015")).to eq(expected)
+      end
+    end
+
+    describe "::team_id_shots_goals_ratio - helper" do
+      it "divides teams shots by goals, creates new hash with team_id pointing to ratio - 20132014" do
+        expected = {
+          30 => 3.5,
+          21 => 3.33,
+          26 => 2.88,
+          24 => 3.23
+        }
+        expect(GameTeam.team_id_shots_goals_ratio("20132014")).to eq(expected)
+      end
+
+      it "divides teams shots by goals, creates new hash with team_id pointing to ratio - 20142015" do
+        expected = {
+          14 => 3.0,
+          19 => 3.7,
+          3 => 3.13,
+          30 => 2.91,
+          5 => 5.0,
+          8 => 5.4,
+          9 => 4.7
+        }
+        expect(GameTeam.team_id_shots_goals_ratio("20142015")).to eq(expected)
+      end
+    end
+
+    describe "::team_id_to_team_name - helper" do
+      it "changes keys from team_id to team_name - 20132014" do
+        expected = {
+          "FC Cincinnati"=>2.88, 
+          "Orlando City SC"=>3.5, 
+          "Real Salt Lake"=>3.23, 
+          "Vancouver Whitecaps FC"=>3.33
+        }
+
+        expect(GameTeam.team_id_to_team_name("20132014")).to eq(expected)
+      end
+
+      it "changes keys from team_id to team_name - 20142015" do
+        expected = {
+          "DC United"=>3.0, 
+          "Houston Dynamo"=>3.13, 
+          "New York City FC"=>4.7, 
+          "New York Red Bulls"=>5.4, 
+          "Orlando City SC"=>2.91, 
+          "Philadelphia Union"=>3.7, 
+          "Sporting Kansas City"=>5.0
+        }
+
+        expect(GameTeam.team_id_to_team_name("20142015")).to eq(expected)
+      end
+    end
+
+    describe "::most_accurate_team" do
+      it "returns the name of the team with the best ratio of shots to goals for specified season" do
+        expect(GameTeam.most_accurate_team("20132014")).to eq("FC Cincinnati")
+        expect(GameTeam.most_accurate_team("20142015")).to eq("Orlando City SC")
+      end
+    end
+
+    describe "::least_accurate_team" do
+      it "returns the name of the team with the worst ratio of shots to goals for specified season" do
+        expect(GameTeam.least_accurate_team("20132014")).to eq("Orlando City SC") # Houston Dash has same ratio as NYC FC - 3.79
+        expect(GameTeam.least_accurate_team("20142015")).to eq("New York Red Bulls")
+      end
+    end
+    
     describe "#total_goals_per_team - helper" do
       it "returns hash of total goals per team" do
         expect(GameTeam.total_goals_per_team).to eq({5=>5, 3=>24, 14=>17, 0=>0, 30=>27, 21=>12, 19=>10, 9=>10, 8=>10, 26=>17, 24=>13})
@@ -162,6 +282,5 @@ RSpec.describe GameTeam do
         expect(GameTeam.worst_offense).to eq("Sporting Kansas City")
       end
     end
-
   end
 end
