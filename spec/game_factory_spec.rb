48917384f8f92d50ad1ff31f6090665dfbc19c55
Noah Durbin <13364668+noahdurbin@users.noreply.github.com>
2024-04-22 08:21:38 -0600

    fix: refactoring

--- spec/game_factory_spec.rb
+++ spec/game_factory_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe GameFactory do
   describe 'creates Game objects from CSV data' do
     it 'creates Game objects' do
       factory = GameFactory.new
-      games = factory.create_games("./fixtures/games_fixture.csv")
+      factory.create_games("./fixtures/games_fixture.csv")
 
       expect(games).to all be_a(Game)
     end
